bf1dd1dc44de55ee044d71f30128d83e394a7bc6
Nika <nika.gamkrelidze.1@btu.edu.ge>
2023-12-26 16:47:29 +0400

    deploy footer

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("c8ca140d61407a17")
+/******/ 		__webpack_require__.h = () => ("7266aef128d01353")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
